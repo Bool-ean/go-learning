4cd1909cbc0289078cc35d74be14aba86a555151
Kyle Jensen <5516255+Kyle-Jensen8@users.noreply.github.com>
2025-12-05 20:15:33 -0800

    1-35 complete. Getting better at concurrency

---
+++ go-by-example/32-channel_directions/channel_directions.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+
+func ping(pings chan<- string, msg string) {
+	pings <- msg
+}
+
+func pong(pongs <-chan string, pings chan<- string) {
+	msg := <- pongs
+	pings <- msg
+}
+
+func main() {
+	pings := make(chan string, 1)
+	pongs := make(chan string, 1)
+	ping(pings, "passed message")
+	pong(pings, pongs)
+	fmt.Println(<-pongs)
+}
