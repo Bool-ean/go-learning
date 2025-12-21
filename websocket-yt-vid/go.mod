1ed7194bb6e7d4434fb11e908da4253917408df2
Kyle Jensen <5516255+Bool-ean@users.noreply.github.com>
2025-12-20 20:21:43 -0800

    websocket more prog

---
+++ websocket-yt-vid/go.mod
@@ -0,0 +1,5 @@
+module github.com/Bool-ean/go-learning/websocket-yt-vid
+
+go 1.25.5
+
+require golang.org/x/net v0.48.0
---
+++ websocket-yt-vid/main.go
@@ -0,0 +1,70 @@
+// code from: https://www.youtube.com/watch?v=JuUAEYLkGbM
+// copying/messing around with it for learning purposes
+//left off the vid at 10:58
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+
+	"golang.org/x/net/websocket"
+)
+type Server struct {
+	//map of websocket connections so we can keep track of connections to server
+	conns map[*websocket.Conn]bool
+
+	//since small example, using mutex instead of channel
+	mu sync.RWMutex
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn){
+
+	defer func() {
+		s.mu.Lock()
+		delete(s.conns, ws)
+		s.mu.Unlock()
+		ws.Close()
+	}()
+
+	fmt.Println("new incoming connection from client:", ws.RemoteAddr())
+
+	//making sure only one thread access's map at the same time
+	s.mu.Lock()
+	s.conns[ws] = true
+	s.mu.Unlock()
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn){
+	
+	buf := make([]byte, 1024)
+	for{
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF{
+				break
+			}
+			fmt.Println("Read error:", err)
+			//continueing so connection isn't loss if error
+			continue
+		}
+		//only reading what was written
+		msg := buf[:n]
+		fmt.Println(string(msg))
+		ws.Write([]byte("thank you for the message"))
+	}
+}
+
+func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.ListenAndServe(":3000", nil)
+}
